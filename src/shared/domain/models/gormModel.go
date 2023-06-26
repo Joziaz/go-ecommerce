7438815b40c64ed93de167b116cb6a8e2c6ecd05
Jozi <josiasduran51@gmail.com>
2023-06-26 18:37:58 -0400

    fix: set gorm default flag correctly to ID field

--- src/shared/domain/models/gormModel.go
+++ src/shared/domain/models/gormModel.go
@@ -7,7 +7,7 @@ import (
 )
 
 type GormModel struct {
-	ID        string `gorm:"primaryKey;default=gen_random_uuid()"`
+	ID        string `gorm:"primaryKey;default:gen_random_uuid()"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
 	DeletedAt gorm.DeletedAt `gorm:"index"`
